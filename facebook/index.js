cffa9c0428c34f3c965130a76b3de0ed13d6564e
Craig Duncan <git@duncanc.co.uk>
2015-12-28 11:14:17 +0000

    Convert the facebook plugin to use jpm

--- facebook/index.js
+++ facebook/index.js
@@ -2,6 +2,6 @@ var data = require("sdk/self").data;
 var pageMod = require("sdk/page-mod");
 
 pageMod.PageMod({
-	include: "https://www.facebook.com/",
-	contentScriptFile: data.url("facebook.js")
+    include: "https://www.facebook.com/",
+    contentScriptFile: data.url("facebook.js")
 });
