c54b2d679832b16ba8322715341febfeb5d4a9fe
Craig Duncan <git@duncanc.co.uk>
2015-12-29 18:24:39 +0000

    Update to work with recent facebook changes

--- facebook/data/facebook.js
+++ facebook/data/facebook.js
@@ -1,10 +1,10 @@
 var $ = document.querySelector.bind(document)
 
 $("#pagelet_ego_pane").remove()
-$("#rightCol").setAttribute("style","width:400px;position:absolute;right:0px;z-index:100;")
+$("#rightCol").setAttribute("style","width:380px;position:absolute;right:0px;z-index:100;")
 $("#contentArea").style.width = "600px"
 $("#pagelet_rhc_footer").remove()
 
-var home = $("#navHome");
-home.nextSibling.remove()
-home.remove()
+// Remove the extra home link, we already have the F logo to get home
+var home = $("div[data-click='home_icon']");
+home.parentNode.remove()
